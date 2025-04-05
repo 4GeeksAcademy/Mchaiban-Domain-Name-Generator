846a3805d11a3d78ecccda2628b79b0a5d220768
Mchaiban <marianafeels@gmail.com>
2025-04-05 00:46:05 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ['the', 'a', 'that', 'any'];
+  let adjectives = ['mystical', 'blue', 'radiant', 'tall'];
+  let nouns = ['castle', 'book', 'galaxy', 'butterfly'];
+  let extensions = ['.com', '.net', '.us']
+
+  for(let article = 0; article < articles.length; article++) {
+    for(let adjective = 0; adjective < adjectives.length; adjective++) {
+      for(let noun = 0; noun < nouns.length; noun++) {
+        for (let extension = 0; extension < extensions.length; extension ++) {
+          console.log(articles [article] + adjectives[adjective] + nouns[noun]+ extensions[extension]); 
+        }
+      }
+    }
+  }
 };
